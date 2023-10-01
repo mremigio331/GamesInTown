40f40805b818e6867a99da6dc6b5595bd6ec90c4
Matt Remy <m.remigio331@gmail.com>
2023-10-01 13:55:18 -0700

    Onboarded all sports to the ESPN API

--- src/api/api_calls.js
+++ src/api/api_calls.js
@@ -5,12 +5,14 @@ import { AllTeams } from '../Data/AllTeamsInfo';
 
 export const getAllGames = async (start_date, end_date, metroAreaTeams) => {
     let allGamesReturn = [];
-    const mlb = await MLBAPI(start_date, end_date, metroAreaTeams);
-    const nhl = await NHLAPI(start_date, end_date, metroAreaTeams);
-    const nfl = await nflGrab(start_date, end_date, metroAreaTeams);
+    const mlb = await FullGamesReturn(start_date, end_date, metroAreaTeams, 'baseball', 'mlb'); //MLBAPI(start_date, end_date, metroAreaTeams);
+    const nhl = await FullGamesReturn(start_date, end_date, metroAreaTeams, 'hockey', 'nhl');
+    const nfl = await FullGamesReturn(start_date, end_date, metroAreaTeams, 'football', 'nfl'); //NFLGrab(start_date, end_date, metroAreaTeams);
+    const nba = await FullGamesReturn(start_date, end_date, metroAreaTeams, 'basketball', 'nba');
     mlb.map((game) => allGamesReturn.push(game));
     nhl.map((game) => allGamesReturn.push(game));
     nfl.map((game) => allGamesReturn.push(game));
+    nba.map((game) => allGamesReturn.push(game));
 
     allGamesReturn.sort((a, b) => {
         const dateA = new Date(a.dateTime);
@@ -20,25 +22,8 @@ export const getAllGames = async (start_date, end_date, metroAreaTeams) => {
     return allGamesReturn;
 };
 
-export const getGames = (start_date, end_date, metroAreaTeams) => {
-    const { isLoading } = useSelector((state) => state.tableReducer);
-    getAllGames(start_date, end_date, metroAreaTeams);
-
-    if (isLoading)
-        return {
-            isLoading: isLoading,
-            error: error,
-            allGames: [],
-        };
-    return {
-        isLoading: isLoading,
-        error: error,
-        allGames: data,
-    };
-};
-
 // NFL Functions
-const nflGrab = async (start_date, end_date, metroAreaTeams) => {
+const NFLGrab = async (start_date, end_date, metroAreaTeams) => {
     const weeksGood = WeeksIdentifer(start_date, end_date);
     let nflGames = [];
     await Promise.all(
@@ -159,12 +144,12 @@ const MLBScrubber = (metroAreaTeams, response) => {
                       homeTeam: {
                           teamName: homeTeam,
                           record: `${homeRecord.wins}-${homeRecord.losses}`,
-                          logo: AllTeams[homeTeam].logo,
+                          logo: AllTeams.hasOwnProperty(homeTeam) ? AllTeams[homeTeam].logo : null,
                       },
                       awayTeam: {
                           teamName: awayTeam,
                           record: `${awayRecord.wins}-${awayRecord.losses}`,
-                          logo: AllTeams[awayTeam].logo,
+                          logo: AllTeams.hasOwnProperty(awayTeam) ? AllTeams[awayTeam].logo : null,
                       },
                       league: 'MLB',
                       fullInfo: game,
@@ -176,19 +161,28 @@ const MLBScrubber = (metroAreaTeams, response) => {
 };
 
 // NHL Functions
-const NHLAPI = async (start_date, end_date, metroAreaTeams) => {
-    const requestURL = `https://statsapi.web.nhl.com/api/v1/schedule??sportId=1&startDate=${start_date}&endDate=${end_date}`;
-
-    const response = await axios
-        .get(requestURL)
-        .then((res) => {
-            return res;
-        })
-        .catch((error) => {
-            return error;
-        });
-
-    return NHLScrubber(metroAreaTeams, response.data);
+const FullGamesReturn = async (start_date, end_date, metroAreaTeams, sport, league) => {
+    //const requestURL = `https://statsapi.web.nhl.com/api/v1/schedule??sportId=1&startDate=${start_date}&endDate=${end_date}`;
+    //
+    //const response = await axios
+    //    .get(requestURL)
+    //    .then((res) => {
+    //        return res;
+    //    })
+    //    .catch((error) => {
+    //        return error;
+    //    });
+    //
+    //return NHLScrubber(metroAreaTeams, response.data);
+    const datesLookup = DatesIdentifier(start_date, end_date);
+    let games = [];
+    await Promise.all(
+        datesLookup.map(async (date) => {
+            const dateGames = await ESPNAPI(sport, league.toLowerCase(), date);
+            dateGames.events.map((game) => games.push(game));
+        }),
+    );
+    return ESPNScrubber(metroAreaTeams, games, league.toUpperCase());
 };
 
 const NHLScrubber = (metroAreaTeams, response) => {
@@ -241,4 +235,88 @@ const TimeZoneIdentifier = (metroAreaTeams, specificTeam) => {
         return metroAreaTeams.teamName == specificTeam;
     });
     return highlightTeamInfo[0].timeZone;
+};
+
+// NBA Functions
+const NBAGrab = async (start_date, end_date, metroAreaTeams) => {
+    const datesLookup = DatesIdentifier(start_date, end_date);
+    let nbaGames = [];
+    await Promise.all(
+        datesLookup.map(async (date) => {
+            const nbaDate = await ESPNAPI('basketball', 'nba', date);
+            nbaDate.events.map((game) => nbaGames.push(game));
+        }),
+    );
+    return ESPNScrubber(metroAreaTeams, nbaGames, 'NBA');
+};
+
+const DatesIdentifier = (start_date, end_date) => {
+    for (var dates = [], dt = new Date(start_date); dt <= new Date(end_date); dt.setDate(dt.getDate() + 1)) {
+        dates.push(new Date(dt).toISOString().split('T')[0].replaceAll('-', ''));
+    }
+    return dates;
+};
+
+const ESPNAPI = async (sport, league, date) => {
+    const requestURL = `http://site.api.espn.com/apis/site/v2/sports/${sport}/${league}/scoreboard?calendartype=whitelist&limit=100&dates=${date}`;
+    console.log(requestURL);
+    const response = await axios
+        .get(requestURL)
+        .then((res) => {
+            return res;
+        })
+        .catch((error) => {
+            return error;
+        });
+
+    return response.data;
+};
+
+const ESPNScrubber = (metroAreaTeams, games, league) => {
+    let Teams = metroAreaTeams.filter(function (metroAreaTeams) {
+        return metroAreaTeams.league == league;
+    });
+    Teams = Teams.map((team) => team.teamName);
+    let AllGames = [];
+    games.map((game) => {
+        const homeTeam = game.competitions[0].competitors[0].team.displayName;
+        const awayTeam = game.competitions[0].competitors[1].team.displayName;
+        const homeRecord = () => {
+            try {
+                return game.competitions[0].competitors[0].records[0].summary;
+            } catch {
+                return null;
+            }
+        };
+        const awayRecord = () => {
+            try {
+                return game.competitions[0].competitors[1].records[0].summary;
+            } catch {
+                return null;
+            }
+        };
+
+        Teams.includes(homeTeam)
+            ? AllGames.push({
+                  dateTime: new Date(game.date).toLocaleString('en-US', {
+                      timeZone: TimeZoneIdentifier(metroAreaTeams, homeTeam),
+                  }),
+                  location: game.competitions[0].venue.fullName,
+                  homeTeam: {
+                      teamName: homeTeam,
+                      record: homeRecord,
+                      logo: game.competitions[0].competitors[0].team.logo,
+                  },
+                  awayTeam: {
+                      teamName: awayTeam,
+                      record: awayRecord,
+                      logo: game.competitions[0].competitors[1].team.logo,
+                  },
+                  league: league,
+                  fullInfo: game,
+              })
+            : null;
+    });
+    console.log(AllGames);
+    return AllGames;
 };
